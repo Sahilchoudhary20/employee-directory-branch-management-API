66a008cc67a57ed892dfecb4ee18fa171a61456a
Sahilchoudhary20 <choudharysahil4309@gmail.com>
2025-10-24 21:48:00 +0530

    added server.ts

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// src/server.ts
+import app from "./app";
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+export default server;
